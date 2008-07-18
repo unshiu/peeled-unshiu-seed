a5e378cb080d1367ef014f911772093e551bac4c
asami <asami@6b3104e4-9f14-0410-8847-ebdc78b6de3d>
2008-07-18 10:15:14 +0000

    test_util.rbは基礎
    git-svn-id: svn+ssh://svn.drecom.co.jp/usr/local/svnrepos/unshiu/seed/trunk@62382 6b3104e4-9f14-0410-8847-ebdc78b6de3d

---
+++ lib/test_util.rb
@@ -0,0 +1,63 @@
+#
+# 単体テスト用Utilモジュール
+# テストクラスにincludeして利用する
+#
+# ex)  class Hoge < Test::Unit::TestCase
+#         include TestUtil::Base::PcControllerTest
+#      end
+#
+module TestUtil
+  
+  module Base
+    
+    # PCのコントローラテスト便利モジュール
+    module PcControllerTest 
+      include AuthenticatedTestHelper
+      
+      def uploaded_file(path, content_type, filename)
+        t = Tempfile.new(filename);
+        FileUtils.copy_file(path, t.path)
+        (class << t; self; end).class_eval do
+          alias local_path path
+          define_method(:original_filename) {filename}
+          define_method(:content_type) {content_type}
+        end
+        return t
+      end
+    end
+
+    class ActionController::TestRequest
+      attr_accessor :user_agent
+    end
+  
+    # 携帯のコントローラテスト便利モジュール
+    # 3キャリアのUser-Agentでテストをまわす
+    module MobileControllerTest 
+      include AuthenticatedTestHelper
+      
+      def setup
+        @request    = ActionController::TestRequest.new
+        @response   = ActionController::TestResponse.new
+        case @count
+        when 0:
+          @request.user_agent = 'DoCoMo/2.0 SH903i(c100;TB;W24H16)'
+        when 1:
+          @request.user_agent = 'KDDI-SA31 UP.Browser/6.2.0.7.3.129 (GUI) MMP/2.0'
+        when 2:
+          @request.user_agent = 'SoftBank/1.0/910T/TJ001'
+        end
+      end
+
+      def run(result)
+        for @count in 0..2
+          super
+        end
+      end
+
+      def login(base_user_id = 1)
+        @controller.send(:current_base_user=, BaseUser.find(base_user_id))
+      end
+
+    end
+  end
+end
