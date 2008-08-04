4bf93255a70c6d6ddfb74311728c2f41bb24db8d
asami_akiko <asami_akiko@6b3104e4-9f14-0410-8847-ebdc78b6de3d>
2008-08-04 06:43:37 +0000

    add base mail address form
    git-svn-id: svn+ssh://svn.drecom.co.jp/usr/local/svnrepos/unshiu/seed/trunk@62556 6b3104e4-9f14-0410-8847-ebdc78b6de3d

---
+++ app/models/mail_address_form.rb
@@ -0,0 +1,11 @@
+require 'active_form'
+
+class MailAddressForm < ActiveForm
+  attr_accessor :mail_address
+
+  validates_presence_of :mail_address
+  validates_legal_mail_address_of :mail_address, :if => Proc.new{|form| !form.mail_address.blank?}
+
+  _("mail address form")
+  _("MailAddressForm|Mail address")
+end
