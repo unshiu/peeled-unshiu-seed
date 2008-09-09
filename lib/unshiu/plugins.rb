7ec6a9e814912bc18746c1356edc1945ca384f1f
asami_akiko <asami_akiko@6b3104e4-9f14-0410-8847-ebdc78b6de3d>
2008-09-09 05:25:26 +0000

    plugin管理lib追加
    git-svn-id: svn+ssh://svn.drecom.co.jp/usr/local/svnrepos/unshiu/seed/trunk@62887 6b3104e4-9f14-0410-8847-ebdc78b6de3d

---
+++ lib/unshiu/plugins.rb
@@ -0,0 +1,19 @@
+module Unshiu
+  module Plugins
+    
+    # plugin の有無を確認する
+    # ex) Unshiu::Plugins.base?
+    def self.method_missing(name, *args)
+      name = name.to_s.scan(/\w+\\?/) 
+      false if name.empty? || name.size != 1
+      File.directory?("#{RAILS_ROOT}/vendor/plugins/#{name[0]}") ? true : false
+    end
+    
+    # unshiu plugin リスト
+    LIST = ['base', 'abm', 'dia', 'prf', 'msg', 'pnt', 'cmm', 'mlg', 'mng', 'tpc']
+    
+    # 現在有効なplugin
+    ACTIVE_LIST = LIST.delete_if { |plugin| eval("!#{plugin}?") }
+    
+  end
+end
