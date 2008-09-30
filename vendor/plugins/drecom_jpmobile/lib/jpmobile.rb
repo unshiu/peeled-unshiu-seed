e01c0685ebc245b764899b859a26d5f7ef14434a
asami_akiko <asami_akiko@6b3104e4-9f14-0410-8847-ebdc78b6de3d>
2008-09-30 05:36:34 +0000

    一部のプラグインの管理方法変更
    git-svn-id: svn+ssh://svn.drecom.co.jp/usr/local/svnrepos/unshiu/seed/trunk@63225 6b3104e4-9f14-0410-8847-ebdc78b6de3d

--- vendor/plugins/drecom_jpmobile/lib/jpmobile.rb
+++
@@ -1,6 +0,0 @@
-#Dir[File.join(File.dirname(__FILE__), 'jpmobile/**/*.rb')].sort.each { |lib| require lib }
-Dir[File.join(File.dirname(__FILE__), 'jpmobile/**/*.rb')].sort.each {|lib|
-  if(lib.index('dispatcher.rb') == nil)
-    require lib
-  end
-}
