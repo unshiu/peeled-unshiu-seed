e931e7f0ba809f97251336f7de12c6f509c52b20
asami_akiko <asami_akiko@6b3104e4-9f14-0410-8847-ebdc78b6de3d>
2008-10-02 09:41:25 +0000

    deployまわり現状を一旦整理
    git-svn-id: svn+ssh://svn.drecom.co.jp/usr/local/svnrepos/unshiu/seed/trunk@63285 6b3104e4-9f14-0410-8847-ebdc78b6de3d

---
+++ config/deployments/sample/recipes.rb
@@ -0,0 +1,51 @@
+Capistrano::Configuration.instance.load do
+  ENVIRONMENT = "sample"
+  
+  set :deployment_code, ENVIRONMENT
+  
+  env_path = []
+  env_path << '/usr/local/bin:/usr/local/mysql/bin'
+  env_path << '$PATH'
+  default_environment['PATH'] = env_path.join(':')
+
+  # -----------------------------------------------------------
+  # DB
+  # -----------------------------------------------------------
+  set :db_password, "root"
+  set :db_user, "root"
+  set :db_host, "127.0.0.1"
+  
+  # -----------------------------------------------------------
+  # Mongrel
+  # -----------------------------------------------------------
+  set :mongrel_rails, File.join(current_path, "script", "mongrel")
+  set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
+  set :mongrel_clean, true
+  set :mongrel_user, "drecom"
+  set :mongrel_group, "drecom"
+  set :mongrel_servers, "2"          # mongrel起動数
+  set :mongrel_environment, ENVIRONMENT
+  set :mongrel_port, "3000"
+  set :mongrel_address, "127.0.0.1"
+  
+  # -----------------------------------------------------------
+  # Memcache
+  # -----------------------------------------------------------
+  set :memcache_server, "127.0.0.1"
+
+  # -----------------------------------------------------------
+  # Backgroundrb
+  # -----------------------------------------------------------
+  set :backgroundrb_environment, ENVIRONMENT
+  set :backgroundrb_host, "127.0.0.1"
+  
+  #set :gateway, "reference3.drecom.jp"
+  #set :ssh_options, :forward_agent => true, :port => 10022
+  set :server_ssh_port, 22
+  
+  role :app,        "127.0.0.1", :port => server_ssh_port
+  role :web,        "127.0.0.1", :port => server_ssh_port
+  role :db,         "127.0.0.1", :primary => true, :port => server_ssh_port
+  role :memcached,  "127.0.0.1", :port => server_ssh_port
+  role :backgroundrb, "127.0.0.1", :port => server_ssh_port
+end
