99c50efed1156f796be4dbf73c9a0b61a29989b1
asami_akiko <asami_akiko@6b3104e4-9f14-0410-8847-ebdc78b6de3d>
2008-12-05 04:03:29 +0000

    文字列から容量変換できるように
    git-svn-id: svn+ssh://svn.drecom.co.jp/usr/local/svnrepos/unshiu/seed/trunk@64715 6b3104e4-9f14-0410-8847-ebdc78b6de3d

--- lib/string_expanse.rb
+++ lib/string_expanse.rb
@@ -8,4 +8,20 @@ class String
     s = "　\s\v"
     self =~ /^[#{s}]+([^#{s}]+)[#{s}]+$/ ? $1 : self
   end
+  
+  def to_byte_i
+    hits = self.scan(/([0-9]+)(K|M|G|T)$/)
+    unless hits.empty?
+      case hits.flatten[1]
+        when "K"
+          hits.flatten[0].to_f * 1024
+        when "M"
+          hits.flatten[0].to_f * 1024 * 1024
+        when "G"
+          hits.flatten[0].to_f * 1024 * 1024 * 1024
+        when "T"
+          hits.flatten[0].to_f * 1024 * 1024 * 1024 * 1024      
+      end
+    end
+  end
 end
