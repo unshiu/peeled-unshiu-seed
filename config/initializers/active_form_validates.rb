1ccbe9de1d899a3314f1d35032bf00154082a0af
asami_akiko <asami_akiko@6b3104e4-9f14-0410-8847-ebdc78b6de3d>
2009-01-21 09:01:26 +0000

    YYYY/MM/DDだとinvalidになってしまので
    git-svn-id: svn+ssh://svn.drecom.co.jp/usr/local/svnrepos/unshiu/seed/trunk@65938 6b3104e4-9f14-0410-8847-ebdc78b6de3d

--- config/initializers/active_form_validates.rb
+++ config/initializers/active_form_validates.rb
@@ -6,3 +6,33 @@ class ActiveForm
   
   include ValidatesDateTime 
 end
+
+# YYYY/MM/DD というフォーマットも対応可能にする
+# http://d.hatena.ne.jp/kusakari/20080409/1207675321
+module ActiveRecord
+  module ConnectionAdapters
+    class Column
+      class << self
+        def string_to_date(value)
+          return if value.blank?
+          return value if value.is_a?(Date)
+          return value.to_date if value.is_a?(Time) || value.is_a?(DateTime)
+          
+          year, month, day = case value.strip
+            # 2006/01/01
+            when /\A(\d{4})\/(\d{1,2})\/(\d{1,2})\Z/
+              [$1, $2, $3]
+            # 2006-01-01
+            when /\A(\d{4})-(\d{1,2})-(\d{1,2})\Z/
+              [$1, $2, $3]
+            # Not a valid date string
+            else
+              return nil
+          end
+          
+          Date.new(unambiguous_year(year), month_index(month), day.to_i) rescue nil
+        end
+      end
+    end
+  end
+end
