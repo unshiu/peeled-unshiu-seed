7e52a32e014f60586cbe7700d84a3a7cb3396c59
asami_akiko <asami_akiko@6b3104e4-9f14-0410-8847-ebdc78b6de3d>
2009-03-18 09:52:46 +0000

    設定もきりかえ
    git-svn-id: svn+ssh://svn.drecom.co.jp/usr/local/svnrepos/unshiu/seed/trunk@69030 6b3104e4-9f14-0410-8847-ebdc78b6de3d

--- config/environment.rb
+++ config/environment.rb
@@ -71,4 +71,4 @@ end
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register "application/x-mobile", :mobile
-
+ActiveReload::ConnectionProxy::setup!
