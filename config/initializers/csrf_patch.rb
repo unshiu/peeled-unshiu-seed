ad51bf6e68d35bea0b3d34dab98784f4a8007bbc
asami_akiko <asami_akiko@6b3104e4-9f14-0410-8847-ebdc78b6de3d>
2009-03-18 11:11:07 +0000

    いらない
    git-svn-id: svn+ssh://svn.drecom.co.jp/usr/local/svnrepos/unshiu/seed/trunk@69034 6b3104e4-9f14-0410-8847-ebdc78b6de3d

--- config/initializers/csrf_patch.rb
+++
@@ -1,4 +0,0 @@
-# CSRFに対する脆弱性に対するパッチ
-# http://weblog.rubyonrails.com/2008/11/18/potential-circumvention-of-csrf-protection-in-rails-2-1
-# rails 2.1.3 or 2.2.2 以降では必要ない
-#Mime::Type.unverifiable_types.delete(:text)
