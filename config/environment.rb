cc6cb94af73deee5112577116d7430bb8bb45c26
asami_akiko <asami_akiko@6b3104e4-9f14-0410-8847-ebdc78b6de3d>
2009-03-19 08:21:01 +0000

    余計なのが紛れ込んでた
    git-svn-id: svn+ssh://svn.drecom.co.jp/usr/local/svnrepos/unshiu/seed/trunk@69106 6b3104e4-9f14-0410-8847-ebdc78b6de3d

--- config/environment.rb
+++ config/environment.rb
@@ -10,8 +10,6 @@ RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 require 'string_expanse'
-require 'cgi'
-require 'cgi/session'
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here
