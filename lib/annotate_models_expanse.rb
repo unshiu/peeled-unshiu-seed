40e69953390e423fc2d6409cf31c1a5c1889ba34
gaooh <silver.vine.ebisen@gmail.com>
2009-09-02 20:24:28 +0900

    scheme infoにバージョン番号を出さないように

--- lib/annotate_models_expanse.rb
+++ lib/annotate_models_expanse.rb
@@ -17,6 +17,26 @@ module AnnotateModels
     end
   end
 
+  def self.do_annotations
+    header = PREFIX.dup
+    
+    self.get_model_names.each do |m|
+      class_name = m.sub(/\.rb$/,'').camelize
+      begin
+        klass = class_name.split('::').inject(Object){ |klass,part| klass.const_get(part) }
+        if klass < ActiveRecord::Base && !klass.abstract_class?
+          puts "Annotating #{class_name}"
+          self.annotate(klass, header)
+        else
+          puts "Skipping #{class_name}"
+        end
+      rescue Exception => e
+        puts "Unable to annotate #{class_name}: #{e.message}"
+      end
+      
+    end
+  end
+  
   # unshiuプラグイン用のannotation作成を実行する
   # 対象をプラグインlibのmodelにしているだけで挙動はdo_annotationsと同様
   # _param1_:: unshiuプラグイン名
